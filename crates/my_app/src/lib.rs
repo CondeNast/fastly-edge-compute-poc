0001f9764e286f5d02919b6162da2c97c9844ce0
Stef <stef.jones@condenast.com>
2020-10-22 13:01:46 +0100

    use str for response body

--- crates/my_app/src/lib.rs
+++ crates/my_app/src/lib.rs
@@ -1,18 +1,18 @@
 use http::{Method, Request, Response, StatusCode};
 
-pub fn render(req: Request<String>) -> Result<Response<String>, http::Error> {
+pub fn render(req: Request<String>) -> Result<Response<&'static str>, http::Error> {
     match req.method() {
         &Method::GET => match req.uri().path() {
             "/" => Ok(Response::builder()
                 .status(StatusCode::OK)
-                .body(String::from("Welcome to Stef's Computer On The Edge v4!"))?),
+                .body("Welcome to Stef's Computer On The Edge v4!")?),
             _ => Ok(Response::builder()
                 .status(StatusCode::NOT_FOUND)
-                .body(String::from("The page you requested could not be found"))?),
+                .body("The page you requested could not be found")?),
         },
         _ => Ok(Response::builder()
             .status(StatusCode::METHOD_NOT_ALLOWED)
-            .body(String::from("This method is not allowed"))?),
+            .body("This method is not allowed")?),
     }
 }
 
