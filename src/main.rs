74e9f173b49afae702ff69d8e101b79d1c3d71e3
Stef <stef.jones@condenast.com>
2020-10-22 13:17:02 +0100

    make it a bit more readable

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,6 @@ use std::convert::TryFrom;
 fn main(req: fastly::Request<fastly::Body>) -> Result<impl fastly::ResponseExt, fastly::Error> {
     let (parts, body) = req.into_parts();
     let req = http::Request::from_parts(parts, body.into_string());
-    Ok(fastly::Response::try_from(my_app::render(req)?)?)
+    let res = my_app::render(req)?;
+    Ok(fastly::Response::try_from(res)?)
 }
