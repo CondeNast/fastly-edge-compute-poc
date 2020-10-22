a7e4697b79fd714574fc7360809b158108cc3de0
Stef <stef.jones@condenast.com>
2020-10-22 11:42:58 +0100

    code cleanup

--- crates/my_app/src/lib.rs
+++ crates/my_app/src/lib.rs
@@ -1,6 +1,6 @@
 use http::{Response, StatusCode};
 
-pub fn get_root() -> Result<Response<&'static str>, http::Error> {
+pub fn render_root() -> Result<Response<&'static str>, http::Error> {
     Response::builder()
         .status(StatusCode::OK)
         .body("Welcome to Stef's Computer On The Edge v3!")
@@ -12,7 +12,9 @@ mod tests {
 
     #[test]
     fn it_responds_with_appropriate_body() {
-        let result = *get_root().unwrap().body();
-        assert_eq!(result, "Welcome to Stef's Computer On The Edge v3!");
+        assert_eq!(
+            render_root().unwrap().body(),
+            &"Welcome to Stef's Computer On The Edge v3!"
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,15 @@ fn fastly_response_from_std(res: http::Response<&str>) -> Result<Response<Body>,
 
 #[fastly::main]
 fn main(req: Request<Body>) -> Result<impl ResponseExt, Error> {
-    if req.method() != Method::GET {
-        Ok(Response::builder()
-            .status(StatusCode::METHOD_NOT_ALLOWED)
-            .body(Body::from("This method is not allowed"))?)
-    } else {
-        match (req.method(), req.uri().path()) {
-            (&Method::GET, "/") => Ok(fastly_response_from_std(my_app::get_root()?)?),
+    match req.method() {
+        &Method::GET => match req.uri().path() {
+            "/" => Ok(fastly_response_from_std(my_app::render_root()?)?),
             _ => Ok(Response::builder()
                 .status(StatusCode::NOT_FOUND)
                 .body(Body::from("The page you requested could not be found"))?),
-        }
+        },
+        _ => Ok(Response::builder()
+            .status(StatusCode::METHOD_NOT_ALLOWED)
+            .body(Body::from("This method is not allowed"))?),
     }
 }
