99a0711be2d87d2ee0ec83bba688e8840e0f4422
Stef <stef.jones@condenast.com>
2020-10-22 11:56:55 +0100

    use fastly::response::try_from

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,18 @@
 use fastly::http::{Method, StatusCode};
 use fastly::{Body, Error, Request, Response, ResponseExt};
-
-fn fastly_response_from_std(res: http::Response<&str>) -> Result<Response<Body>, Error> {
-    match Response::builder()
-        .status(res.status())
-        .body(Body::from(*res.body()))
-    {
-        Ok(res) => Ok(res),
-        Err(error) => Err(Error::from(error)),
-    }
-}
+use std::convert::TryFrom;
 
 #[fastly::main]
 fn main(req: Request<Body>) -> Result<impl ResponseExt, Error> {
     match req.method() {
         &Method::GET => match req.uri().path() {
-            "/" => Ok(fastly_response_from_std(my_app::render_root()?)?),
+            "/" => Ok(Response::try_from(my_app::render_root()?)?),
             _ => Ok(Response::builder()
                 .status(StatusCode::NOT_FOUND)
-                .body(Body::from("The page you requested could not be found"))?),
+                .body("The page you requested could not be found")?),
         },
         _ => Ok(Response::builder()
             .status(StatusCode::METHOD_NOT_ALLOWED)
-            .body(Body::from("This method is not allowed"))?),
+            .body("This method is not allowed")?),
     }
 }
