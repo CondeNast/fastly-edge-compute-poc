11649c6989a7d19a9919f094cf7b7edebc9f05a0
Stef <stef.jones@condenast.com>
2020-10-22 13:13:12 +0100

    use try_from instead of match

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 use fastly::ResponseExt;
+use std::convert::TryFrom;
 
 #[fastly::main]
 fn main(req: fastly::Request<fastly::Body>) -> Result<impl fastly::ResponseExt, fastly::Error> {
     let (parts, body) = req.into_parts();
     let req = http::Request::from_parts(parts, body.into_string());
-    let res = my_app::render(req);
-    match res {
-        Ok(res) => Ok(res),
-        Err(error) => Err(fastly::Error::from(error)),
-    }
+    Ok(fastly::Response::try_from(my_app::render(req)?)?)
 }
