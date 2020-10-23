06a7544e4c4329c007f029c6a6a0c4fae54892f4
Stef <stef.jones@condenast.com>
2020-10-23 14:43:20 +0100

    patches from upstream repo

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use fastly::ResponseExt;
-use std::convert::TryFrom;
 
 #[fastly::main]
 fn main(req: fastly::Request<fastly::Body>) -> Result<impl fastly::ResponseExt, fastly::Error> {
+    // convert fastly::Request to standard http::Request
     let (parts, body) = req.into_parts();
     let req = http::Request::from_parts(parts, body.into_string());
-    let res = app::render(req)?;
-    Ok(fastly::Response::try_from(res)?)
+
+    // render app
+    let res: http::Response<String> = app::render(req)?;
+
+    // convert response back to fastly::Response
+    let res: fastly::Response<String> = res.into();
+    Ok(res)
 }
